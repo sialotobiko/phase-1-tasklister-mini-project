cb3d7be602686a84d98be173fb7053ed86e29458
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-08-25 01:56:33 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildList(e.target.new_task.value);
+    
+    form.reset();
+  })
+})
+
+
+const buildList = (todo) => {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'X';
+  p.textContent = `${todo}`;
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
